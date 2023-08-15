56397e5cc54318f2fbb81e41cfb43caf75a4152b
James Liu <jliu@sirokygroup.com>
2023-08-15 10:14:52 -0400

    refactor index creation

--- Chat/Utils/Constants.cs
+++ Chat/Utils/Constants.cs
@@ -1,5 +1,9 @@
 ﻿namespace Chat.Utils {
     public class Constants {
         public const string cst_UnknowUser = "Unknown User";
+
+
+        //mongodb index names IDX_[Table]_[Field]
+        public const string IDX_USER_UserId = "idx_user_userId";
     }
 }
--- Chat/Utils/MongoDb/MongoDbUserService.cs
+++ Chat/Utils/MongoDb/MongoDbUserService.cs
@@ -13,6 +13,9 @@ namespace Chat.Utils.MongoDb {
             MongoClient client = new MongoClient(MongoDbConfig.ConnectionString);
             IMongoDatabase database = client.GetDatabase(MongoDbConfig.DatabaseName);
             _userCollection = database.GetCollection<User>(MongoDbConfig.UserCollectionName);
+
+
+            CreateUniqueIndexOnUserId();
         }
 
         public async Task<User> GetUserAsync(string UserId) {
@@ -24,19 +27,22 @@ namespace Chat.Utils.MongoDb {
         }
 
         public async Task CreateUserAsync(User user) {
-            await CreateUniqueIndexOnUserId();
             await _userCollection.InsertOneAsync(user);
         }
 
 
-        private async Task CreateUniqueIndexOnUserId() {
+        #region Index creation
+        private void CreateUniqueIndexOnUserId() {
             var indexModel = new CreateIndexModel<User>(
                 new IndexKeysDefinitionBuilder<User>().Ascending(x => x.UserId),
-                new CreateIndexOptions() { Unique = true, Collation = new Collation("en", strength: CollationStrength.Primary) } // Case-Insensitive index
+                new CreateIndexOptions() { Unique = true, Name=Constants.IDX_USER_UserId, Background = true, Collation = new Collation("en", strength: CollationStrength.Primary) } // Case-Insensitive index
              );
 
-            await _userCollection.Indexes.CreateOneAsync(indexModel);
+            var indexes = _userCollection.Indexes.List().ToList().Select(x => x.GetElement("name").Value.ToString()).ToList();
+            if (indexes.Any(x => x!=null && x.Equals(Constants.IDX_USER_UserId))) {
+                _userCollection.Indexes.CreateOne(indexModel);
+            }            
         }
-
+        #endregion
     }
 }
