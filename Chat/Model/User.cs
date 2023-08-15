52bf9660aab4a4f34dd75dfd067de4d4a2992185
James Liu <jliu@sirokygroup.com>
2023-08-15 19:50:30 -0400

    one more model

--- Chat/Model/User.cs
+++ Chat/Model/User.cs
@@ -9,13 +9,23 @@ namespace Chat.Model {
         public string UserId { get; set; }
         public string Password { get; set; }      
         public string Name { get; set; }
+        public Gender Gender { get; set; } 
+        public string? Province { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
         public string? Email { get; set; }
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
-
         public User(string UserId,string Password,string Name) {
             this.UserId = UserId;
             this.Password = Password;
             this.Name = Name;
         }
+    }
+
+    public enum Gender {
+        unknown,
+        male,
+        female 
     }
 }
---
+++ Chat/Model/UserFriend.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Model {
+    public class UserFriend {
+        public string UserId { get; set; }
+        public string FriendUserId { get; set; }
+        public UserFriendStatus FriendStatus { get; set; } = UserFriendStatus.Requested;
+        public DateTime RequestedAt { get; set; } = DateTime.Now;
+        public DateTime AcceptOrDeniedAt { get; set; }
+        public FriendRequestMessage? MessageIn { get; set; }
+        public FriendRequestMessage? MessageOut { get; set; }
+        public bool Blocked { get; set; } = false;
+
+        public UserFriend(string userId, string friendUserId) {
+            UserId = userId;
+            FriendUserId = friendUserId;
+        }
+    }
+
+    public enum UserFriendStatus {
+        Requested,
+        Pending, //send message back and forth to confirm friend
+        Accepted,
+        Denied
+    }
+
+    public class FriendRequestMessage {
+        public string? Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
