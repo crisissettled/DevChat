de4c5163b6cf8e02053ed158a9f062463b31eaba
James Liu <jliu@sirokygroup.com>
2023-09-04 08:49:44 -0400

    nav active class

--- Chat/ClientApp/src/components/nav/NavMenuChat.js
+++ Chat/ClientApp/src/components/nav/NavMenuChat.js
@@ -1,5 +1,5 @@
 ﻿import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import './NavMenu.css';
 
@@ -7,6 +7,13 @@ import { Logo } from '../logo/Logo'
 
 export function NavMenuChat() {
     const user = useSelector(state => state.user)
+
+    const location = useLocation();
+
+    const getClassName = (path) => {  
+        return location.pathname === path || (path === "/" && location.pathname === "/chat") ? "text-dark border-bottom border-info border-2" : "text-dark";
+    }
+
     return (
         <header>
             <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
@@ -15,16 +22,16 @@ export function NavMenuChat() {
                 <Collapse className="d-sm-inline-flex flex-sm-row-reverse" navbar>
                     <ul className="navbar-nav flex-grow">
                         <NavItem>
-                            <NavLink tag={Link} className="text-dark" to="/">Chat</NavLink>
+                            <NavLink tag={Link} className={getClassName("/")} to="/" >Chat</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink tag={Link} className="text-dark" to="/findfriend">Find Friend</NavLink>
+                            <NavLink tag={Link} className={getClassName("/findfriend")} to="/findfriend">Find Friend</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink tag={Link} className="text-dark" to="/profile">Profile</NavLink>
+                            <NavLink tag={Link} className={getClassName("/profile")} to="/profile">Profile</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink tag={Link} className="text-dark" to="/signout">Sign Out</NavLink>
+                            <NavLink tag={Link} className={getClassName("/signout")} to="/signout">Sign Out</NavLink>
                         </NavItem>
                         <NavItem>
                             <a href="/swagger" target="_blank">swagger</a>
