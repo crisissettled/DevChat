d1b891cde0fb0da6ef6ade90fc4d141e68a658c3
James Liu <jliu@sirokygroup.com>
2023-11-25 19:20:29 -0500

    docker compose

--- Chat/Model/Configs/MongoDbConfig.cs
+++ Chat/Model/Configs/MongoDbConfig.cs
@@ -2,7 +2,7 @@
 {
     public class MongoDbConfig
     {
-        public static string ConnectionString = "mongodb://localhost:27017";
+        public static string ConnectionString = "mongodb://db:27017";
         public static string DatabaseName = "dev_chat";
         public static string UserCollectionName = "user";
         public static string UserSessionStateCollectionName = "user_session_state";
