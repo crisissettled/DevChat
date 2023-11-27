9aa2f95b0dcaf09fd4730dca4eac9aab24169e93
James Liu <jliu@sirokygroup.com>
2023-11-27 06:45:33 -0500

    add swagger authrization

--- Chat/Program.cs
+++ Chat/Program.cs
@@ -6,13 +6,38 @@ using Chat.Utils.Middleware;
 using Chat.Utils.MongoDb;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddSignalR();
 builder.Services.AddMemoryCache();
 builder.Services.AddAuthentication(options => {
@@ -20,37 +45,36 @@ builder.Services.AddAuthentication(options => {
     // However, we want JWT Bearer Auth to be the default.
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-       .AddJwtBearer(options => {
-           options.RequireHttpsMetadata = false;
-           options.SaveToken = true;
-           options.TokenValidationParameters = new TokenValidationParameters {
-               ValidateIssuerSigningKey = true,
-               IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Jwt.AccessSecret)),
-               ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256 },
-               ValidIssuer = Jwt.Issuer,
-               ValidAudience = Jwt.Audience,
-               ValidateIssuer = true,
-               ValidateAudience = true,
-               ValidateLifetime = true,
-               ClockSkew = TimeSpan.FromSeconds(0)
-           };
-           options.Events = new JwtBearerEvents {
-               OnMessageReceived = context => {
-                   var accessToken = context.Request.Query["access_token"];
-                   var path = context.HttpContext.Request.Path;
-                   if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs/chat")) {
-                       context.Token = accessToken;
-                   }
-                   return Task.CompletedTask;
-               },
-               OnAuthenticationFailed = context => {
-                   if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                       context.Response.Headers.Add("tokenErr", "expired");
-                   return Task.CompletedTask;
-               }
-           };
-       });
+}).AddJwtBearer(options => {
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Jwt.AccessSecret)),
+        ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256 },
+        ValidIssuer = Jwt.Issuer,
+        ValidAudience = Jwt.Audience,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromSeconds(0)
+    };
+    options.Events = new JwtBearerEvents {
+        OnMessageReceived = context => {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs/chat")) {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        },
+        OnAuthenticationFailed = context => {
+            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                context.Response.Headers.Add("tokenErr", "expired");
+            return Task.CompletedTask;
+        }
+    };
+});
 
 builder.Services.AddSingleton<ICrypto, Crypto>();
 builder.Services.AddSingleton<IChatSessions, ChatSessions>();
